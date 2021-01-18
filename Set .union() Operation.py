01bee2a4dd6fce25e603d109db0c86b791affac3
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2021-01-19 00:48:48 +0530

    Create Set .union() Operation.py

---
+++ Set .union() Operation.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+ne = int(input())
+setne = set(map(int,input().split()))
+nf = int(input())
+setnf = set(map(int,input().split()))
+set3 = setne.union(setnf)
+print(len(set3))
