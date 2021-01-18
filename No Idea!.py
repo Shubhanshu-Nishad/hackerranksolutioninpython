25e2e449f132ce8432fcbf3c3ec6d7e5b7b771fb
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2021-01-18 10:58:47 +0530

    Create No Idea!.py

---
+++ No Idea!.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n,m = input().split()
+
+
+storage = list()
+count = 0
+
+storage = list(map(int, input().strip().split()))
+
+A = set(map(int, input().strip().split()))
+B = set(map(int, input().strip().split()))
+
+for i in storage:
+    if i in A:
+        count = count+1
+    if i in B:
+        count = count-1
+
+print(count)
