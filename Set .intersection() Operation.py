18ac1e848622f78c5f483753a6ecdc12e9e1b685
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2021-01-19 00:49:56 +0530

    Create Set .intersection() Operation.py

---
+++ Set .intersection() Operation.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+ne = int(input())
+setne = set(map(int,input().split()))
+nf = int(input())
+setnf = set(map(int,input().split()))
+set3 = setne.intersection(setnf)
+print(len(set3))
