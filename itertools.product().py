5d25e657da6c5e69cb44409575adf4c231d177fa
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2021-01-22 23:52:02 +0530

    Create itertools.product().py

---
+++ itertools.product().py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import product
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+
+ls=list(product(a,b))
+for i in ls:
+    print(i,end=" ")
