ea176c9c4c38609e10c956d254a12f426fb73423
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2021-01-23 00:24:08 +0530

    Create Maximize It!.py

---
+++ Maximize It!.py
@@ -0,0 +1,12 @@
+import itertools
+k,m = map(int, input().split())
+l = []
+for i in range(k):
+  l.append([x*x for x in list(map(int, input().split()))[1:]])
+ 
+s = 0
+for e in itertools.product(*l):
+  temp = sum(e)%m
+  if temp > s:
+    s = temp
+print(s)
